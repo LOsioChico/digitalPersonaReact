0a058a6af3292fd2da286c2cd1209fc294c043ee
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2024-12-01 11:29:12 -0500

    fix: Update file paths for WebSdk script sources

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['src/modules/WebSdk/index.js']
-  }
-})
+  resolve: {
+    alias: {
+      WebSdk: path.resolve(dirname(""), "./src/modules/WebSdk/index.js"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: ["/WebSdk.js"],
+    },
+  },
+});
